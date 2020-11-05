b944b26c1585a7e8d9d34df205758b9e5aae575a
inzapp <inzapp@naver.com>
2020-11-05 16:23:15 +0900

    Solve DummyData(?) : check mmconvert --inputShape

--- convert.sh
+++ convert.sh
@@ -1,3 +1,4 @@
 python3 h5_to_pb.py
-mmconvert -sf tf -iw model.pb -df caffe -om model --inputShape=386,640,1 --inNodeName=x --dstNodeName=Identity
+cp -afv log/model.pb model.pb
+mmconvert -sf tf -iw model.pb -df caffe -om model --inputShape=368,640,1 --inNodeName=x --dstNodeName=Identity --dump_tag=SERVING
 rm -r __pycache__
--- h5_to_pb.py
+++ h5_to_pb.py
@@ -6,25 +6,25 @@ from tensorflow.python.framework.convert_to_constants import convert_variables_t
 
 
 def convert_h5_to_frozen_pb():
-    model = tf.keras.models.load_model('model.h5', compile=False)
-    tf.keras.backend.set_learning_phase(0)
+    model = tf.keras.models.load_model('model.h5', compile=True)
+#    tf.keras.backend.set_learning_phase(0)
     full_model = tf.function(lambda x: model(x))
     full_model = full_model.get_concrete_function(tf.TensorSpec(model.inputs[0].shape, model.inputs[0].dtype))
 
     frozen_func = convert_variables_to_constants_v2(full_model)
     frozen_func.graph.as_graph_def()
 
-    layers = [op.name for op in frozen_func.graph.get_operations()]
-    print("-" * 50)
-    print("Frozen model layers: ")
-    for layer in layers:
-        print(layer)
-
-    print("-" * 50)
-    print("Frozen model inputs: ")
-    print(frozen_func.inputs)
-    print("Frozen model outputs: ")
-    print(frozen_func.outputs)
+#    layers = [op.name for op in frozen_func.graph.get_operations()]
+#    print("-" * 50)
+#    print("Frozen model layers: ")
+#    for layer in layers:
+#        print(layer)
+#
+#    print("-" * 50)
+#    print("Frozen model inputs: ")
+#    print(frozen_func.inputs)
+#    print("Frozen model outputs: ")
+#    print(frozen_func.outputs)
 
     tf.io.write_graph(
         graph_or_graph_def=frozen_func.graph,
@@ -34,8 +34,8 @@ def convert_h5_to_frozen_pb():
     )
 
     net = cv2.dnn.readNet(r'model.pb')
-    for layer_name in net.getLayerNames():
-        print(layer_name)
+#    for layer_name in net.getLayerNames():
+#        print(layer_name)
 
 
 def convert_pb_to_caffemodel():
