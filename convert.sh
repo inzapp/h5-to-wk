dbf7edf666de36428c238473f15abea06b4cb8c5
inzapp <inzapp@naver.com>
2020-12-23 21:43:37 -0500

    c

--- convert.sh
+++ convert.sh
@@ -1,3 +1,3 @@
 export PATH=/home/kali/.local/bin:$PATH
-#mmconvert -sf tf -iw model.h5 -df caffe -om model --inputShape=368,640,1 --inNodeName=x --dstNodeName=Identity 
-mmconvert -sf tf -iw model.h5 -df caffe -om model --inNodeName=x --dstNodeName=Identity
+mmconvert -sf tf -iw model.h5 -df caffe -om model --inputShape=368,640,1 --inNodeName=x --dstNodeName=Identity 
+#mmconvert -sf tf -iw model.h5 -df caffe -om model --inNodeName=x --dstNodeName=Identity
--- mapper/run_mapper.sh
+++ mapper/run_mapper.sh
@@ -1,5 +1,5 @@
 export LD_LIBRARY_PATH="./:${LD_LIBRARY_PATH}"
 ./nnie_mapper_12 ./config.cfg
-cp -avf ../model.wk ~/nfs/target_hi3519a/home/sbd.wk
-ls -lrt ~/nfs/target_hi3519a/home/
+#cp -avf ../model.wk ~/nfs/target_hi3519a/home/sbd.wk
+#ls -lrt ~/nfs/target_hi3519a/home/
 date
