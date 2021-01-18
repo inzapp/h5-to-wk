f1090813db5b95d26264fd7b099c70da82d8249a
inzapp <inzapp@naver.com>
2021-01-18 01:04:02 -0500

    c

--- mapper/run_mapper.sh
+++ mapper/run_mapper.sh
@@ -1,5 +1,8 @@
 export LD_LIBRARY_PATH="./:${LD_LIBRARY_PATH}"
 ./nnie_mapper_12 ./config.cfg
+rm ../*.pb
+rm ../*.npy
+np ../*.json
 #cp -avf ../model.wk ~/nfs/target_hi3519a/home/sbd.wk
 #ls -lrt ~/nfs/target_hi3519a/home/
-date
+#date
